37460a268b40730a20d3e28c046b76dc0d567a8f
aliyetgin <yetgin.ali@outlook.com>
2023-06-14 17:34:25 +0300

    Initial commit

---
+++ src/main/java/com/aliyetgin/TechCareer_FullStack/TechCareerFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.aliyetgin.TechCareer_FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCareerFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliyetgin/TechCareer_FullStack/TechCareerFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliyetgin.TechCareer_FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
