0b451de99ee6ef82bc274dd35e248ac03e98d1dd
aliyetgin <yetgin.ali@outlook.com>
2023-06-15 14:24:50 +0300

    Little tweaks

--- src/main/java/com/aliyetgin/business/dto/BlogDto.java
+++ src/main/java/com/aliyetgin/business/dto/BlogDto.java
@@ -1,4 +1,34 @@
 package com.aliyetgin.business.dto;
 
-public class BlogDto {
+import com.aliyetgin.audit.AuditingAwareBaseDto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+// LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+
+// BlogDTo
+public class BlogDto extends AuditingAwareBaseDto implements Serializable {
+    public static final Long serialVersionUID = 1L;
+
+    @NotEmpty(message = "header boş geçilemez")
+
+    @NotEmpty(message = "{blog.header.validation.constraints.NotNull.message}")
+    @Size(min = 10, message = "{blog.header.least.validation.constraints.NotNull.message}")
+    private String header;
+
+    @NotEmpty(message = "{blog.content.validation.constraints.NotNull.message}")
+    @Size(min = 10, message = "{blog.content.least.validation.constraints.NotNull.message}")
+    private String content;
 }
